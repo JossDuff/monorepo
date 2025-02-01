2c089045e392e82911ca17743a6f1993cff95a1a
jossduff <jduff360@gmail.com>
2025-02-01 11:10:39 -0500

    feat: span + agg proof in minimal-server

--- sigil/op-succinct/proposer/succinct/bin/minimal-server.rs
+++ sigil/op-succinct/proposer/succinct/bin/minimal-server.rs
@@ -18,27 +18,25 @@ pub const AGG_ELF: &[u8] = include_bytes!("../../../elf/aggregation-elf");
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let l2_start_block = 250551;
-    let l2_end_block = 250556;
+    let l2_start_block = 12500;
+    let l2_end_block = 12501;
+
     // dummy payload
     let payload = SpanProofRequest {
-        // start: 113137,
-        // end: 113437
         start: l2_start_block,
-        end: l2_end_block, // start: 250330,
-                           // end: 250331
+        end: l2_end_block, 
     };
+
     utils::setup_logger();
 
     dotenv::dotenv().ok();
 
     let prover = ProverClient::from_env();
     let (range_pk, range_vk) = prover.setup(RANGE_ELF);
-    let (_agg_pk, agg_vk) = prover.setup(AGG_ELF);
+    // let (_agg_pk, agg_vk) = prover.setup(AGG_ELF);
     let multi_block_vkey_u8 = u32_to_u8(range_vk.vk.hash_u32());
     let _range_vkey_commitment = B256::from(multi_block_vkey_u8);
-    let _agg_vkey_hash = B256::from_str(&agg_vk.bytes32()).unwrap();
-
+    // let _agg_vkey_hash = B256::from_str(&agg_vk.bytes32()).unwrap();
     let fetcher = match OPSuccinctDataFetcher::new_with_rollup_config(RunContext::Docker).await {
         Ok(f) => f,
         Err(e) => {
@@ -111,12 +109,18 @@ async fn main() -> Result<()> {
         .expect("saving proof failed");
 
     info!("saved proof to {}", proof_path);
+    drop(prover);
+    info!("sleeping for 10 seconds");
+    tokio::time::sleep(tokio::time::Duration::from_secs(10)).await;
+    info!("good morning");
+    let proof_path = "proofs/12500-12501.bin".to_string();
+
+    let (proofs, boot_infos) = load_aggregation_proof_data(proof_path);
+
+    info!("loaded saved proof");
 
     let prover = ProverClient::from_env();
     let (_, vkey) = prover.setup(RANGE_ELF);
-    let (proofs, boot_infos) = load_aggregation_proof_data(proof_path, &vkey);
-
-    info!("loaded saved proof");
 
     let header = fetcher.get_latest_l1_head_in_batch(&boot_infos).await?;
     let headers = fetcher
@@ -124,6 +128,7 @@ async fn main() -> Result<()> {
         .await?;
     let multi_block_vkey_u8 = u32_to_u8(vkey.vk.hash_u32());
     let multi_block_vkey_b256 = B256::from(multi_block_vkey_u8);
+
     // println!(
     //     "Range ELF Verification Key Commitment: {}",
     //     multi_block_vkey_b256
@@ -150,10 +155,7 @@ async fn main() -> Result<()> {
 /// Load the aggregation proof data.
 fn load_aggregation_proof_data(
     proof_path: String,
-    range_vkey: &SP1VerifyingKey,
 ) -> (Vec<SP1Proof>, Vec<BootInfoStruct>) {
-
-    let prover = ProverClient::builder().cpu().build();
 
     if fs::metadata(&proof_path).is_err() {
         panic!("Proof file not found: {}", proof_path);
@@ -161,10 +163,6 @@ fn load_aggregation_proof_data(
 
     let mut deserialized_proof =
         SP1ProofWithPublicValues::load(proof_path).expect("loading proof failed");
-
-    prover
-        .verify(&deserialized_proof, range_vkey)
-        .expect("proof verification failed");
 
     // The public values are the BootInfoStruct.
     let boot_info = deserialized_proof.public_values.read();
